c86f2de48bea9d05e5c29039dff7a4d673f1163a
marcela <marcela@192.168.24.33>
2015-05-21 19:24:30 -0500

    Agregamos ultimas modificacion de modulo lecciones

--- BackboneTest/public_html/js/views/lesson/lessonStepView.js
+++ BackboneTest/public_html/js/views/lesson/lessonStepView.js
@@ -2,22 +2,34 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'text!templates/lesson/lessonStepTemplate.html'
-], function($, _, Backbone, lessonStepTemplate){
+  'text!templates/lesson/lessonStepTemplate.html',
+  'models/StepModel',
+  'models/LessonModel'
+], function($, _, Backbone, lessonStepTemplate,StepModel,LessonModel){
 
   var LessonStepView = Backbone.View.extend({
-    el: $("#container"),
+    el: $("#container"),   
     render: function(idCategory, idLesson, idStep){
-
+        var lesson = new LessonModel({idlesson:idLesson});
+    
+        var step = new StepModel({idstep:idStep});
+        step.sync("read",step,{success: function (step) {
+                    $('#reto').html(lesson.step.challengue),
+                    $('#pista').html(lesson.step.code),
+                    $('#puntaje').html(lesson.step.points);
+                    $('#topmenu2').html(lesson.step.name);
+                }});
         var that = this;
-        //..
-        var data = {};
+                 
+        var data = {step:step};
         var compiledTemplate = _.template( lessonStepTemplate, data );
+            
         $("#container").html(compiledTemplate);
     }
-
   });
-
-  return LessonStepView;
   
+        
+ 
+     
+  return LessonStepView;
 });
