299a67be87cb1801f5df9f49b05b4e107c3578d1
JhonPolitecnico <jhonpolitecnico@gmail.com>
2015-10-25 01:46:49 -0500

    Implementada validación de un reto

--- BackboneTest/public_html/js/views/lesson/lessonStepView.js
+++ BackboneTest/public_html/js/views/lesson/lessonStepView.js
@@ -13,14 +13,28 @@ define([
         el: $("#container"),
         render: function (idCategory, idLesson, idStep) {
 
+            var code, regex, func;
+            var editor;
 
             var lesson = new LessonModel({idcategory: idCategory, idlesson: idLesson});
             var step = new StepModel({idstep: idStep});
+
             step.sync("read", step,
                     {success: function (step) {
+
+                            code = step.code;
+                            func = step.eval;
+                            regex = step.expression;
+
+                            editor = ace.edit("editor");
+                            editor.setOptions({
+                                maxLines: Infinity,
+                                theme: "ace/theme/twilight",
+                                mode: "ace/mode/java"
+                            });
+
                             $('#reto').html(step.challenge);
-                            $('#pista').html(step.code);
-                            ace.edit("editor").session.setValue(step.code);
+                            editor.session.setValue(step.code);
                             $('#puntaje').html(step.points);
                             $('#topmenu2').html(step.name);
                         }
@@ -30,6 +44,31 @@ define([
             var data = {step: step};
             var compiledTemplate = _.template(lessonStepTemplate, data);
             $("#container").html(compiledTemplate);
+
+            $('#validate').click(function () {
+
+                var myRegexp = new RegExp(regex, 'gi');
+                var match = myRegexp.exec(editor.getValue());
+
+                var real_func = eval('[' + func + ']')[0];
+
+                if (match !== null) {
+
+                    var args = [];
+
+                    for (var i = 1; i < match.length; i++) {
+                        args.push(match[i]);
+                    }
+                    if (real_func.apply(this, args) === true) {
+                        alert('Pasaste el reto!');
+                        return;
+                    }
+                }
+
+                alert('Muy mal, sigue intentando!');
+
+            });
+
         }
     });
 
